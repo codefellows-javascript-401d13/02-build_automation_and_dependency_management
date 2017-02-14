ef660dccfda2a50dbc1cdad2a422464c80f32969
Dana Kulp <dkulp23@gmail.com>
2017-02-14 15:45:43 -0800

    Continuing to explore the chai expect testing capabilities.

--- test/howdy-test.js
+++ test/howdy-test.js
@@ -2,29 +2,29 @@
 
 const howdy = require('../lib/howdy.js');
 const expect = require('chai').expect;
-// const hackyMessageVariable = howdy.multipleGreetings.logMessage;
+const hackyMessageVariable = howdy.multipleGreetings.logMessage;
 // console.log(hackyMessageVariable);
 
 describe('Howdy Module', function() {
   describe('#greeting', function() {
     it(`should return hello ${process.argv[2]}`, function() {
       let printout = howdy.greeting(process.argv[2]);
-      // assert.ok(printout === `hello ${process.argv[2]}`, 'incorrect result');
       expect(howdy).to.have.property('greeting');
+      expect(printout).to.match(/^[a-zA-Z,!\s]+/);
       expect(printout).to.eql(`hello ${process.argv[2]}`);
     });
+
+    it('Should result in error from missing input', function() {
+      let fn = howdy.greeting;
+      expect(fn).to.throw(Error);
+    });
   });
-  // describe('#greeting', function() {
-  //   it('Should result in error from missing input', function() {
-  //     assert.throws(function() {
-  //       howdy.greeting();
-  //     }, 'Error not thrown for empty input');
-  //   });
-  // });
-  // describe('#multipleGreetings', function() {
-  //   it('should return all names entered in command line', function() {
-  //     let printout = howdy.multipleGreetings(process.argv);
-  //     assert.ok(printout === `Hey there ${hackyMessageVariable}`, 'not quite right');
-  //   });
-  // });
+
+  describe('#multipleGreetings', function() {
+    it('should return all names entered in command line', function() {
+      let printout = howdy.multipleGreetings(process.argv);
+      // assert.ok(printout === `Hey there ${hackyMessageVariable}`, 'not quite right');
+      expect(printout).to.match(/^[a-zA-Z,!\s]+/);
+    });
+  });
 });
