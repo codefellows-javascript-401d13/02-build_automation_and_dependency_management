c21adb3cae87f6facf364def9e83c3ae078c7918
Zachary Crumbo <zcrumbo@mac.com>
2017-02-14 15:42:57 -0800

    Added sayBye function to greet.js and added test to check it

--- test/greet-test.js
+++ test/greet-test.js
@@ -6,12 +6,18 @@ const expect = require('chai').expect;
 describe('Greet Module', function(){
   describe('#greet', function(){
     it('should output hello Zachary', function(){
-      var result = greet('Zachary');
+      var result = greet.sayHey('Zachary');
       expect(result).to.equal('hello Zachary');
     });
     it('should throw error if no string passed', function(){
-      var error = greet;
+      var error = greet.sayHey;
       expect(error).to.throw(Error);
+    });
+  });
+  describe('#sayBye', function(){
+    it('should output see ya later', function(){
+      var result = greet.sayBye();
+      expect(result).to.equal('see ya later!');
     });
   });
 });
