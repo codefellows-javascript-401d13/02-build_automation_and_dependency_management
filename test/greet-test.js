f3cdc70c8929141ebf2bf79fc5a75adedd077610
Gary Lundgren <builtbygl@gmail.com>
2017-02-15 00:08:42 -0800

    fix

--- test/greet-test.js
+++ test/greet-test.js
@@ -1,32 +1,27 @@
 'use strict';
 
 const greet = require('../lib/greet.js');
-const assert = require('assert');
+const expect = require('chai').expect;
 
 describe('Greet Module', function() {
   describe('#sayHi', function() {
     it('should return Hello Gary!', function() {
       var result = greet.sayHi('Gary');
-      assert.ok(result === 'Hello Gary!', 'not equal to Hello Gary!');
+      expect(greet).to.have.property('sayHi');
+      expect(result).to.equal('Hello Gary!');
     });
 
     it('should throw a missing name error', function() {
-      assert.throws(function() {
-        greet.sayHi();
-      }, 'error not thrown');
+      var result = greet.sayHi;
+      expect(result).to.throw(Error);
     });
   });
+
   describe('#sayBye', function() {
     it('should return See you later!', function() {
-      var bye = 'See you later!';
-      assert.ok(bye === 'See you later!', 'not equal to See you later!');
+      var result = greet.sayBye();
+      expect(result).to.equal('See you later!')
     });
   });
-  // attempt to test my command line argument, not sure how to do it.
-  // describe('#greetStranger', function() {
-  //   it('should return Hi there, argument name!', function() {
-  //     var stranger = greet.greetStranger(`${greet.strangerName}`);
-  //     assert.ok(stranger === `Hi there, ${greet.strangerName}!`, 'not equal to Hi there, argument name!');
-  //   });
-  // });
+
 });
