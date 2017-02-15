39763525d7f1158836c7fa03b27b6b858c885a66
noahgribbin <noahgribbin@yahoo.com>
2017-02-14 18:15:27 -0800

    add gulpfile and change assert to expect

---
+++ lab-noah/test/greet-test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const greet = require('../lib/greet.js');
+const expect = require('chai').expect;
+
+describe('Greet Module', function(){
+  describe('#sayHey',function(){
+    it('should return hey noah',function(){
+      var result = greet.sayHey('noah');
+      expect(greet).to.have.property('sayHey')
+      expect(result).to.equal('hey noah')
+    });
+    it('should have a missing name error message', function(){
+      var result = greet.sayHey;
+      expect(result).to.throw(Error);
+    });
+  });
+  describe('#sayBye',function(){
+    it('should return see ya later', function(){
+      var result = greet.sayBye();
+      expect(result).to.equal('see ya later');
+    });
+  });
+});
